a4e02f1e0a61725f9a488f8f052b950faead0bbf
rami boussarsar <ramibousarsar@gmail.com>
2024-06-21 18:35:36 -0400

    bunditfix

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,54 @@
 class CarsController < ApplicationController
   def index
-    @cars = Car.all
+    @cars = policy_scope(Car)
+  end
+
+  def show
+    @car = Car.find(params[:id])
+    authorize @car
+  end
+
+  def new
+    @car = Car.new
+    authorize @car
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    authorize @car
+    if @car.save
+      redirect_to @car, notice: 'Car was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+    authorize @car
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    authorize @car
+    if @car.update(car_params)
+      redirect_to @car, notice: 'Car was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    authorize @car
+    @car.destroy
+    redirect_to cars_url, notice: 'Car was successfully destroyed.'
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:car_name, :features, :transmission, :fuel_type, :car_make, :image, :price_per_day, :rating, :number_of_seat)
   end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,54 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = policy_scope(Favorite)
+    authorize @favorites
+  end
+
+  def show
+    @favorite = Favorite.find(params[:id])
+    authorize @favorite
+  end
+
+  def new
+    @favorite = Favorite.new
+    authorize @favorite
+  end
+
+  def create
+    @favorite = Favorite.new(favorite_params)
+    authorize @favorite
+    if @favorite.save
+      redirect_to @favorite, notice: 'Favorite was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @favorite = Favorite.find(params[:id])
+    authorize @favorite
+  end
+
+  def update
+    @favorite = Favorite.find(params[:id])
+    authorize @favorite
+    if @favorite.update(favorite_params)
+      redirect_to @favorite, notice: 'Favorite was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    authorize @favorite
+    @favorite.destroy
+    redirect_to favorites_url, notice: 'Favorite was successfully destroyed.'
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:attribute)
+  end
 end
