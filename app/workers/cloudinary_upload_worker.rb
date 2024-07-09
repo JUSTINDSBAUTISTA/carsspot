a9164f3ede3c8b034e27c94369dd98e7f473d905
rami boussarsar <ramibousarsar@gmail.com>
2024-07-09 16:53:54 -0400

    Precompile assets for production

--- app/workers/cloudinary_upload_worker.rb
+++ app/workers/cloudinary_upload_worker.rb
@@ -1,14 +1,11 @@
-# app/workers/cloudinary_upload_worker.rb
 class CloudinaryUploadWorker
   include Sidekiq::Worker
 
   def perform(car_id, image_url)
-    logger.info "Performing CloudinaryUploadWorker with car_id=#{car_id} and image_url=#{image_url}"
     car = Car.find(car_id)
-    Cloudinary::Uploader.upload(image_url, public_id: car.id)
-  rescue ActiveRecord::RecordNotFound
-    logger.error "Car with id=#{car_id} not found."
+    response = Cloudinary::Uploader.upload(image_url, public_id: car.id)
+    car.update(image_url: response['secure_url']) if response
   rescue => e
-    logger.error "An error occurred: #{e.message}"
+    Rails.logger.error "CloudinaryUploadWorker Error: #{e.message}"
   end
 end
