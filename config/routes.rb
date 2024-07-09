be8c05864cd2467a2ea435091a3528f2f07435d6
rami boussarsar <ramibousarsar@gmail.com>
2024-07-08 22:19:27 -0400

    Add Sidekiq web UI with basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :cars do
+    member do
+      post 'favorite'
+    end
+
     collection do
       get :my_cars
       get :pending_approval
