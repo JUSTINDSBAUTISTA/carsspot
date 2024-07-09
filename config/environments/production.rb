aa221372b8edc0c4c83add197bd8b5cebfcc11ca
rami boussarsar <ramibousarsar@gmail.com>
2024-07-09 16:50:15 -0400

    Precompile assets and fix channel issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -80,7 +80,6 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  # Set the host for URL generation
   config.action_mailer.default_url_options = { host: 'carsspot-1286c883ae12.herokuapp.com' }
   Rails.application.routes.default_url_options[:host] = 'carsspot-1286c883ae12.herokuapp.com'
 end
