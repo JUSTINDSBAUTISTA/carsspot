0fcbe499660a10193b2d46261a032c730659d1e0
rami boussarsar <ramibousarsar@gmail.com>
2024-07-08 23:46:37 -0400

    Add terser gem and configure for production

--- app/workers/cloudinary_upload_worker.rb
+++ app/workers/cloudinary_upload_worker.rb
@@ -4,7 +4,12 @@ class CloudinaryUploadWorker
 
   def perform(car_id, image_url)
     car = Car.find(car_id)
-    Cloudinary::Uploader.upload(image_url, public_id: car.id)
-    Rails.logger.info "Image uploaded to Cloudinary for Car ID: #{car.id}"
+    image_path = ActiveStorage::Blob.service.send(:path_for, car.image.key)
+    Cloudinary::Uploader.upload(image_url, public_id: car.image.filename.base)
+    Rails.logger.info "Image uploaded to Cloudinary: #{car.image.filename}"
+  rescue ActiveRecord::RecordNotFound => e
+    Rails.logger.error "Car with id=#{car_id} not found."
+  rescue StandardError => e
+    Rails.logger.error "An error occurred: #{e.message}"
   end
 end
