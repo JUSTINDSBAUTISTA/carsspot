5cb2d033c4bd4d6c0c598620577f189283cf358e
rami boussarsar <ramibousarsar@gmail.com>
2024-07-09 17:33:38 -0400

    Configure Redis for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -79,7 +79,4 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.default_url_options = { host: 'carsspot-1286c883ae12.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'carsspot-1286c883ae12.herokuapp.com'
 end
