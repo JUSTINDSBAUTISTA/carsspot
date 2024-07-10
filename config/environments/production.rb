3bf4050771645c21626c747d0fc5f411694d87d0
rami boussarsar <ramibousarsar@gmail.com>
2024-07-09 21:19:02 -0400

    Update Active Storage configuration for Cloudinary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-# config/environments/production.rb
+# config/environments/development.rb
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -9,6 +9,9 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
+
+  # Use Cloudinary for Active Storage in development
+  config.active_storage.service = :cloudinary
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -36,7 +39,8 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # Commented out to use Cloudinary
+  # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -68,14 +72,14 @@ Rails.application.configure do
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   config.action_cable.disable_request_forgery_protection = true
 
-  # Action Cable configuration
-  config.action_cable.url = "wss://carsspot-1286c883ae12.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://carsspot-1286c883ae12.herokuapp.com', 'http://carsspot-1286c883ae12.herokuapp.com']
+  # Action Cable configuration for development
+  config.action_cable.url = "ws://localhost:3000/cable"
+  config.action_cable.allowed_request_origins = ['http://localhost:3000', /http:\/\/localhost:\d+/]
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = false
