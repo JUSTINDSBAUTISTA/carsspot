a1b9e3aa57da0e1b8280176bea1a72ddd8b6626c
rami boussarsar <ramibousarsar@gmail.com>
2024-07-09 21:45:22 -0400

    oUpdate Active Storage configuration for Cloudinary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.active_storage.service = :cloudinary
+
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
